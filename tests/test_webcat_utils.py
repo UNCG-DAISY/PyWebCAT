05d6496e768eca280f421ea04793d553d4790853
Tomas Beuzen <tomas.beuzen@gmail.com>
2020-06-10 10:32:01 -0700

    add tests

---
+++ tests/test_webcat_utils.py
@@ -0,0 +1,13 @@
+from webcat_utils import WebCAT
+from pytest import raises
+
+
+def test_generate_url():
+    wc = WebCAT()
+    wc.generate_url("buxtoncoastalcam", 2019, 11, 13, 1000)
+    assert (
+        wc.url
+        == "http://webcat-video.axds.co/buxtoncoastalcam/raw/2019/2019_11/2019_11_13/buxtoncoastalcam.2019-11-13_1000.mp4"
+    )
+    with raises(ValueError):
+        wc.generate_url("fakestation", 2019, 11, 13, 1000)
